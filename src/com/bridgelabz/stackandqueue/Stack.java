5e4f4d2a4286b02f56a6a20251e21be5e0f5a26a
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-16 17:35:58 +0530

    START :- Displayed Welcome message.

---
+++ src/com/bridgelabz/stackandqueue/Stack.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.stackandqueue;
+
+import java.util.LinkedList;
+
+public class Stack {
+	/** created object of LinkedList class **/
+	LinkedList<Integer> stack = new LinkedList<Integer>();
+
+	public static void main(String[] args) {
+		/** Displayed welcome message **/
+		System.out.println("----------WELCOME TO STACK-----------\n");
+	}
+}
